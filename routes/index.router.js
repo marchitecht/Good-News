eaec8246b8233c5130da70fbab2f0249acb74226
Alexaner Piontek <nekom113@mail.ru>
2022-04-07 17:56:49 +0300

    input tags done

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const { Tags } = require('../db/models');
 
 router.route('/')
   .get((req, res) => {
     res.render('index');
   });
+router.post('/', async (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+  // try {
+  //   const tags = await Tags.create({ name });
+  //   res.json(tags);
+  // } catch (error) {
+  //   console.log(error);
+  // }
+});
 module.exports = router;
