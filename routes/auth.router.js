9ca075d52e874cac470a1a33c8c4364cf78ad1f7
Martin <martin.musinn@gmail.com>
2022-04-08 10:02:57 +0300

    this is a fuck up

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,59 +1,59 @@
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const { User } = require('../db/models');
+  const router = require('express').Router();
+  const bcrypt = require('bcrypt');
+  const { User } = require('../db/models');
 
-router.route('/signup')
-  .get((req, res) => {
-    res.render('signup');
-  }).post(async (req, res) => {
-    const { name, email, password } = req.body;
-    if (name && email && password) {
-      const secretPass = await bcrypt.hash(password, 10);
-      try {
-        const newUser = await User.create({ name, email, password: secretPass });
-        res.locals.user = newUser;
-        req.session.user = { id: newUser.id, name: newUser.name };
-        return res.redirect('/');
-      } catch (err) {
-        console.log(err);
-        res.redirect('/');
-      }
-    } else {
-      res.redirect('/');
-    }
-  });
-
-router.route('/signin')
-  .get((req, res) => {
-    res.render('signin');
-  }).post(async (req, res) => {
-    const { email, password } = req.body;
-    if (email && password) {
-      try {
-        const currUser = await User.findOne({
-          where: {
-            email,
-          },
-        });
-        // eslint-disable-next-line max-len
-        if (bcrypt.compare(password, currUser.password)) {
-          req.session.user = { id: currUser.id, name: currUser.name };
-          res.locals.user = currUser;
-          res.redirect('/');
-        } else {
+  router.route('/signup')
+    .get((req, res) => {
+      res.render('signup');
+    }).post(async (req, res) => {
+      const { name, email, password } = req.body;
+      if (name && email && password) {
+        const secretPass = await bcrypt.hash(password, 10);
+        try {
+          const newUser = await User.create({ name, email, password: secretPass });
+          res.locals.user = newUser;
+          req.session.user = { id: newUser.id, name: newUser.name };
+          return res.redirect('/');
+        } catch (err) {
+          console.log(err);
           res.redirect('/');
         }
-      } catch (err) {
-        console.log(err);
+      } else {
         res.redirect('/');
       }
-    } else res.redirect('/');
-    // eslint-disable-next-line max-len
+    });
+
+  router.route('/signin')
+    .get((req, res) => {
+      res.render('signin');
+    }).post(async (req, res) => {
+      const { email, password } = req.body;
+      if (email && password) {
+        try {
+          const currUser = await User.findOne({
+            where: {
+              email,
+            },
+          });
+          // eslint-disable-next-line max-len
+          if (bcrypt.compare(password, currUser.password)) {
+            req.session.user = { id: currUser.id, name: currUser.name };
+            res.locals.user = currUser;
+            res.redirect('/');
+          } else {
+            res.redirect('/');
+          }
+        } catch (err) {
+          console.log(err);
+          res.redirect('/');
+        }
+      } else res.redirect('/');
+      // eslint-disable-next-line max-len
+    });
+
+  router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.clearCookie('sid').redirect('/');
   });
 
-router.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.clearCookie('sid').redirect('/');
-});
-
-module.exports = router;
+  module.exports = router;
