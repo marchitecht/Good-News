68626288e2848d7c002f956fe03e5f773853cc87
Matvey <motich.radchenko@gmail.com>
2022-04-08 12:47:12 +0300

    front

--- routes/index.router.js
+++ routes/index.router.js
@@ -4,16 +4,28 @@ const { Tag, BWlist } = require('../db/models');
 
 router.route('/')
 .get( async (req, res) => {
-  // const whiteList = await BWlist.findAll({where:{userId:req.session.user.id, isGood: true}, include:'Tag' })
-  // console.log(whiteList);
-    res.render('index');
-  });
+  if (req.session.user) {
+    const whiteList = await Tag.findAll({include:{ model:BWlist, where: {userId:req.session.user.id, isGood: true}},raw:true})
+    const blackList = await Tag.findAll({include:{ model:BWlist, where: {userId:req.session.user.id, isGood: false}},raw:true})
+    res.render('index', {whiteList,blackList});
+  } else {
+    res.render('index'); 
+  }
+});
+
 router.post('/', async (req, res) => {
   const isGood = req.body.isGood;
   const userId = req.session.user.id;
   const newTag = await Tag.findOrCreate({where:{name:req.body.tagName}, raw:true});
   const tagId = newTag[0].id;
-  await BWlist.create({tagId,userId,isGood});
+  await BWlist.findOrCreate({where: {tagId,userId,isGood}});
+  res.sendStatus(200);
+});
+
+router.delete('/:name', async function (req, res, next) {
+  const { name } = req.params;
+  const tag = await Tag.findOne({where: {name}, raw: true})
+  await BWlist.destroy({ where: { tagId: tag.id } });
   res.sendStatus(200);
 });
 module.exports = router;
